a53247a449c547cb1c015d4cd2fd0c2d751ec96c
kharthicsrinivasan0204 <kharthicsrinivasan0204@gmail.com>
2025-09-26 09:21:46 +0530

    Add files via upload

---
+++ the greatest of two number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+   int num1, num2;
+   printf("Enter two numbers ");
+   scanf("%d%d", &num1, &num2);
+   if(num1>num2){
+       printf("%d ,is greatest  than %d\n" ,num1, num2);
+     }else if(num2>num1){
+        printf("%d, is greatest than %d\n", num2,num1);
+    }else{
+    printf("Both number are  equal: %d \n",num1);
+    }   
+    
+   
+   
+
+    return 0;
+}
